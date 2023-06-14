900c06c72ee48399b0c4566cabacd4e6aa057bea
BONGSEOK <qhdtjr4517@gmail.com>
2023-06-14 21:45:30 +0900

    로컬 작업 초본  index.js, example.txt 추가

---
+++ 백준/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+let inputStr = fs.readFileSync(filePath).toString();
+
+/** 입력예제 input 값 초기화 */
+// let inputs = inputStr.split(' ');
+
+/** 기능 구현 */
+
+/** solution 함수 정의  */
+function solution() {}
+
+/** solution 함수 호출 */
+solution();
