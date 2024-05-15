2111ae2b9db0e929e2e83ab972106701c7d6d912
bongseoksa <bongseoksa@users.noreply.github.com>
2024-05-15 23:58:27 +0900

    [Bronze V] Title: A×B, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '\\example.txt';
+let inputStr = fs.readFileSync(filePath).toString();
+
+/** 입력예제 input 값 초기화 */
+let inputs = inputStr.split(' ');
+
+/** 기능 구현 */
+
+/** solution 함수 정의  */
+function solution() {
+  console.log(parseInt(inputs[0]) * parseInt(inputs[1]));
+}
+
+/** solution 함수 호출 */
+solution();
