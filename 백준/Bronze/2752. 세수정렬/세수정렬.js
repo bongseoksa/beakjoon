baf88a333ef693cd9d8033e136fcc24a4e3c6baa
bongseoksa <bongseoksa@users.noreply.github.com>
2024-05-20 22:47:38 +0900

    [Bronze IV] Title: 세수정렬, Time: 128 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '\\example.txt';
+let inputStr = fs.readFileSync(filePath).toString();
+
+/** 입력예제 input 값 초기화 */
+let inputs = inputStr.split(' ');
+
+/** 기능 구현 */
+
+/** solution 함수 정의  */
+function solution() {
+  const nums = inputs.map(Number);
+  const sortNums = nums.sort((a, b) => a - b);
+  console.log(sortNums.join(' '));
+}
+
+/** solution 함수 호출 */
+solution();
