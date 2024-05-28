1f10d3d37f9c518bfcf94bf6e2871715d51059f9
bongseoksa <bongseoksa@users.noreply.github.com>
2024-05-28 11:31:23 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 36508 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -8,34 +8,27 @@ let inputStr = fs.readFileSync(filePath).toString().trim();
 let inputs = inputStr.split('\n');
 const N = Number(inputs[0]); // 도시 개수
 const dists = inputs[1].split(' ').map(Number); // 도시간 거리
-const prices = inputs[2].split(' ').map(Number); // 도시별 유가
+let prices = inputs[2].split(' ').map(Number); // 도시별 유가
 
 /** 기능 구현 */
 
 /** solution 함수 정의  */
 function solution() {
-  let totalPrice = 0;
+  let totalPrice = BigInt(0);
   //마지막 주유소 금액 미필요
   prices.pop();
 
-  let leftDist = dists.reduce((a, b) => a + b);
   let minPrice = 1000000001;
-
-  let dist = 0;
-  for (let i = 0; i < N - 1; i++) {
-    //유가가 제일 낮은 곳까지 처리
-
-    //유가가 가장 낮은 곳에서 최종까지 처리
-    if (prices[i] < minPrice) {
-      minPrice = prices[i];
-      dist = 0;
-    }
-
-    dist = dists[i];
-    totalPrice += minPrice * dist;
+  for (let i = 0; i < prices.length; ++i) {
+    if (prices[i] > minPrice) prices[i] = minPrice;
+    else minPrice = prices[i];
   }
 
-  console.log(totalPrice);
+  for (let i = 0; i < N - 1; i++) {
+    totalPrice += BigInt(dists[i]) * BigInt(prices[i]);
+  }
+
+  console.log(String(totalPrice));
 }
 
 /** solution 함수 호출 */
