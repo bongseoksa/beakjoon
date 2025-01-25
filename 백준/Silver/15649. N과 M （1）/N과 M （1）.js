160fae0e0b81056c9d349ec5dc07e8ffda50bc55
bongseoksa <bongseoksa@users.noreply.github.com>
2025-01-25 19:59:11 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 18084 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,41 +1,39 @@
-const fs = require('fs');
-// const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : __dirname + '\\example.txt';
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+// const filePath =
+//   process.platform === "linux" ? "/dev/stdin" : __dirname + "\\example.txt";
 let inputStr = fs.readFileSync(filePath).toString().trim();
 
 /** 입력예제 input 값 초기화 */
-let inputs = inputStr.split('\n');
-const [N, M] = inputs[0].split(' ').map(Number);
-
-let nums = Array.from({ length: N });
-nums = nums.map((num, index) => (num = index + 1));
-let visited = new Array(nums.length).fill(false);
-let selectedNums = [];
+let inputs = inputStr.split(" ");
+let [N, M] = inputs.map(Number);
+let nums = []; // 1~N까지 숫자 배열
+for (let i = 1; i <= N; i++) {
+  nums.push(i);
+}
+let visited = Array(N).fill(false);
+let selected = [];
+let result = "";
 
 /** 기능 구현 */
-function dfs(depth) {
+
+/** solution 함수 정의  */
+function solution(nums, depth) {
   if (depth === M) {
-    console.log(selectedNums.join(' '));
+    result += `${selected.join(" ")}\n`;
     return;
   }
 
   for (let i = 0; i < nums.length; i++) {
     if (visited[i]) continue;
-
-    selectedNums.push(nums[i]);
     visited[i] = true;
-    dfs(depth + 1);
-
-    selectedNums.pop(); // 마지막 선택한 요소 취소
-    visited[i] = false; // 마지막 방문처리 취소
+    selected.push(nums[i]);
+    solution(nums, depth + 1);
+    selected.pop();
+    visited[i] = false;
   }
 }
 
-/** solution 함수 정의  */
-function solution() {
-  dfs(0);
-}
-
 /** solution 함수 호출 */
-solution();
+solution(nums, 0);
+console.log(result.trim());
